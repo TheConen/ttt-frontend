20c873d7270a3352deb68002eafc1db221072b9f
TheConen <tc@theconen.de>
2024-11-22 00:15:18 +0100

    feat: Add header component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideAnimationsAsync(),
-        provideExperimentalZonelessChangeDetection(),
-    ],
+    providers: [provideRouter(routes), provideAnimationsAsync(), provideExperimentalZonelessChangeDetection()],
 };
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+    selector: 'app-header',
+    imports: [MatToolbar, MatButton],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
