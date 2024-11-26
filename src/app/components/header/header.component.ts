1f23d483f22c5715b9f108a1c3eaabed260921e4
TheConen <tc@theconen.de>
2024-11-26 21:47:02 +0100

    feat: Rework header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatButton } from '@angular/material/button';
+import { MatAnchor, MatFabButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbar, MatButton],
+    imports: [MatToolbar, RouterLink, MatAnchor, MatFabButton],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
