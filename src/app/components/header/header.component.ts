7d47b98388e6e991d18a30d8e974b62fe8c2a59d
TheConen <tc@theconen.de>
2024-12-13 23:35:24 +0100

    build: Remove Angular Material, add back zone.js

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatAnchor, MatFabButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [MatToolbar, RouterLink, MatAnchor, MatFabButton],
+    imports: [RouterLink],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
