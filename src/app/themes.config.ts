7f729a8146e0d333832dda812e3e636b756835e0
TheConen <tc@theconen.de>
2024-12-14 23:03:03 +0100

    refactor: Add TTT colors as constants to theme configuration

--- src/app/themes.config.ts
+++ src/app/themes.config.ts
@@ -1,6 +1,12 @@
 import Aura from '@primeng/themes/aura';
 import { definePreset } from '@primeng/themes';
 
+export const tttRed = '#a10f2b';
+export const tttGray = '#40403e';
+export const tttWhite = '#f5f3f2';
+export const tttBlack = '#191919';
+export const tttGreen = '#c6cfba';
+
 export const tttPrimeNgTheme = definePreset(Aura, {
     primitive: {
         red: {
@@ -9,7 +15,7 @@ export const tttPrimeNgTheme = definePreset(Aura, {
             200: '#d798a4',
             300: '#c56a7c',
             400: '#b33d53',
-            500: '#a10f2b',
+            500: tttRed,
             600: '#890d25',
             700: '#710b1e',
             800: '#590818',
@@ -22,11 +28,11 @@ export const tttPrimeNgTheme = definePreset(Aura, {
             200: '#adadac',
             300: '#898987',
             400: '#646463',
-            500: '#40403e',
+            500: tttGray,
             600: '#363635',
             700: '#2d2d2b',
             800: '#232322',
-            900: '#1a1a19',
+            900: tttBlack,
             950: '#101010',
         },
     },
@@ -47,18 +53,18 @@ export const tttPrimeNgTheme = definePreset(Aura, {
         colorScheme: {
             light: {
                 surface: {
-                    0: '#ffffff',
-                    50: '#f5f5f5',
-                    100: '#d1d1d1',
-                    200: '#adadac',
-                    300: '#898987',
-                    400: '#646463',
-                    500: '#40403e',
-                    600: '#363635',
-                    700: '#2d2d2b',
-                    800: '#232322',
-                    900: '#1a1a19',
-                    950: '#101010',
+                    0: tttWhite,
+                    50: '{surface.50}',
+                    100: '{surface.100}',
+                    200: '{surface.200}',
+                    300: '{surface.300}',
+                    400: '{surface.400}',
+                    500: '{surface.500}',
+                    600: '{surface.600}',
+                    700: '{surface.700}',
+                    800: '{surface.800}',
+                    900: '{surface.900}',
+                    950: '{surface.950}',
                 },
                 primary: {
                     color: '{primary.500}',
@@ -66,7 +72,7 @@ export const tttPrimeNgTheme = definePreset(Aura, {
             },
             dark: {
                 surface: {
-                    0: '#ffffff',
+                    0: tttWhite,
                     50: '{surface.50}',
                     100: '{surface.100}',
                     200: '{surface.200}',
