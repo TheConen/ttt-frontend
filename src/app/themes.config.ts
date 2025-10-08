8910031e3748e84b943928daa2d6edeca06ba98a
MenomDE <menom@berlin.de>
2025-10-08 15:32:46 +0200

    fix(header): color override

--- src/app/themes.config.ts
+++ src/app/themes.config.ts
@@ -159,6 +159,7 @@ export const tttPrimeNgTheme: ThemeType = {
                             activeColor: '{primary.500}',
                         },
                     },
+
                 },
             },
         },
